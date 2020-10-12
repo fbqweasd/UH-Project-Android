69dca995734549ba1a4241fd4b18d1c662a2e9f5
fbqweasd <fbqweasd2911@gmail.com>
2020-10-12 21:09:37 +0900

    WOL 적용 메시지 서버에 전송

--- app/src/main/java/com/example/tcp_test/management.kt
+++ app/src/main/java/com/example/tcp_test/management.kt
@@ -8,12 +8,20 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import kotlinx.android.synthetic.main.fragment_management.*
+import kotlinx.android.synthetic.main.fragment_youtube.*
 import java.io.IOError
+import java.io.InputStream
+import java.io.OutputStream
 import java.net.*
 
 class management : Fragment(R.layout.fragment_management) {
 
-    var sock =  DatagramSocket(null)
+    val IP  = "192.168.150.18"
+    val Port = 5657
+    lateinit var InPutStream: InputStream
+    lateinit var OutPutStream: OutputStream
+    lateinit var sock:Socket
+
     val COM_MAC = "00D861C36D40"
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -27,30 +35,42 @@ class management : Fragment(R.layout.fragment_management) {
     inner class SendWOL : Thread() {
         override fun run() {
             
-            try {
-                sock.bind(InetSocketAddress("192.168.150.8",7))
-            }catch (e:BindException){
-                activity?.runOnUiThread {
-                    Toast.makeText(activity, "UDP 브로드캐스트 에러",  Toast.LENGTH_SHORT).show()
-                }
-            }
+//            try {
+//                sock.bind(InetSocketAddress("192.168.150.8",7))
+//            }catch (e:BindException){
+//                activity?.runOnUiThread {
+//                    Toast.makeText(activity, "UDP 브로드캐스트 에러",  Toast.LENGTH_SHORT).show()
+//                }
+//            }
+//
+//            var SendData : ByteArray = "FFFFFFFFFFFF".toByteArray()
+//
+//            for(i in 1..16){
+//                SendData += COM_MAC.toByteArray()
+//            }
+//
+//            var sendPacket = DatagramPacket(SendData, SendData.size)
+//            print(SendData.size)
+//
+//            try {
+//                sock.send(sendPacket)
+//            } catch(e:Exception) {
+//                activity?.runOnUiThread {
+//                    Toast.makeText(activity, "UDP 전송 에러",  Toast.LENGTH_SHORT).show()
+//                }
+//            }
 
-            var SendData : ByteArray = "FFFFFFFFFFFF".toByteArray()
+            sock = Socket(IP, Port)
 
-            for(i in 1..16){
-                SendData += COM_MAC.toByteArray()
-            }
+            InPutStream = sock.getInputStream()
+            OutPutStream = sock.getOutputStream()
 
-            var sendPacket = DatagramPacket(SendData, SendData.size)
-            print(SendData.size)
+            var data = "WOL"
+            var SendData : ByteArray = ByteArray(1)
 
-            try {
-                sock.send(sendPacket)
-            } catch(e:Exception) {
-                activity?.runOnUiThread {
-                    Toast.makeText(activity, "UDP 전송 에러",  Toast.LENGTH_SHORT).show()
-                }
-            }
+            SendData.set(4, 1)
+            OutPutStream.write(SendData + data.toByteArray())
+            OutPutStream.flush()
 
         }
     }
