5164b4063669ddd4391ef09e0393eefd35ca86f9
fbqweasd <fbqweasd2911@gmail.com>
2021-05-20 22:02:15 +0900

    [패킷 수정] MAC 정상적으로 송신 & 종료 패킷 기능 추가

--- app/src/main/java/com/example/tcp_test/management.kt
+++ app/src/main/java/com/example/tcp_test/management.kt
@@ -14,11 +14,7 @@ class management : Fragment(R.layout.fragment_management) {
     val ServerIP  = "ukc.iptime.org"
     val ServerPort = 5657
 
-    lateinit var InPutStream: InputStream
-    lateinit var OutPutStream: OutputStream
-    lateinit var sock:Socket
-
-    val COM_MAC = "00D861C36D40"
+    val COM_MAC : String = "00D861C36D40"
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
 
@@ -26,12 +22,21 @@ class management : Fragment(R.layout.fragment_management) {
             var SendThread = SendWOL()
             SendThread.start()
         }
+
+        PowerOff_Button.setOnClickListener {
+            var ShutDownThread = ShutDownPacket()
+            ShutDownThread.start()
+        }
     }
 
     inner class SendWOL : Thread() {
+        lateinit var InPutStream: InputStream
+        lateinit var OutPutStream: OutputStream
+        lateinit var sock:Socket
+
         override fun run() {
             try {
-                sock = Socket(ServerIP, ServerPort)
+                this.sock = Socket(ServerIP, ServerPort)
             }catch (e:Exception){
                 activity?.runOnUiThread {
                     Toast.makeText(activity, "서버 연결 에러",  Toast.LENGTH_SHORT).show()
@@ -39,16 +44,30 @@ class management : Fragment(R.layout.fragment_management) {
                 return;
             }
 
-            InPutStream = sock.getInputStream()
-            OutPutStream = sock.getOutputStream()
+            this.InPutStream = this.sock.getInputStream()
+            this.OutPutStream = this.sock.getOutputStream()
 
-            var SendData : ByteArray = ByteArray(2)
+            var SendData : ByteArray = ByteArray(8)
             var RecvData : ByteArray = ByteArray(4)
 
             SendData.set(0, 4) // Type Set
             SendData.set(1, 0x6) // Len
-//            System.arraycopy(COM_MAC, 0, SendData, 2, COM_MAC.length)
-//            SendData += COM_MAC.toInt().toByte()
+
+            SendData.set(2, Integer.parseInt(COM_MAC.substring(0,2), 16 ).toByte())
+            SendData.set(3, Integer.parseInt(COM_MAC.substring(2,4), 16 ).toByte())
+            SendData.set(4, Integer.parseInt(COM_MAC.substring(4,6), 16 ).toByte())
+            SendData.set(5, Integer.parseInt(COM_MAC.substring(6,8), 16 ).toByte())
+            SendData.set(6, Integer.parseInt(COM_MAC.substring(8,10), 16 ).toByte())
+            SendData.set(7, Integer.parseInt(COM_MAC.substring(10,12), 16 ).toByte())
+
+//            COM_MAC.substring(0, 1)
+
+//            SendData.set(2, COM_MAC[0].toInt().toByte())
+//            SendData.set(3, COM_MAC[1].toInt().toByte())
+//            SendData.set(4, COM_MAC[2].toInt().toByte())
+//            SendData.set(5, COM_MAC[3].toInt().toByte())
+//            SendData.set(6, COM_MAC[4].toInt().toByte())
+//            SendData.set(7, COM_MAC[5].toInt().toByte())
 
             try {
                 OutPutStream.write(SendData)
@@ -63,6 +82,56 @@ class management : Fragment(R.layout.fragment_management) {
                 else{
                     activity?.runOnUiThread {
                         Toast.makeText(activity, "컴퓨터 부팅 성공",  Toast.LENGTH_SHORT).show()
+                    }
+                }
+
+                sock.close()
+            }catch (e:Exception){
+                activity?.runOnUiThread {
+                    Toast.makeText(activity, "데이터 전송 에러",  Toast.LENGTH_SHORT).show()
+                }
+                sock.close()
+                return;
+            }
+        }
+    }
+
+    inner class ShutDownPacket : Thread() {
+        lateinit var InPutStream: InputStream
+        lateinit var OutPutStream: OutputStream
+        lateinit var sock:Socket
+
+        var SendData : ByteArray = ByteArray(8)
+        var RecvData : ByteArray = ByteArray(4)
+
+        override fun run() {
+            try {
+                this.sock = Socket(ServerIP, ServerPort)
+            } catch (e: Exception) {
+                activity?.runOnUiThread {
+                    Toast.makeText(activity, "서버 연결 에러", Toast.LENGTH_SHORT).show()
+                }
+                return;
+            }
+
+            this.InPutStream = this.sock.getInputStream()
+            this.OutPutStream = this.sock.getOutputStream()
+
+            SendData.set(0, 5) // Type Set
+
+            try {
+                OutPutStream.write(SendData)
+                OutPutStream.flush()
+
+                var RecvSize = InPutStream.read(RecvData)
+                if(RecvSize == 0 || RecvData[0] != 5.toByte()){
+                    activity?.runOnUiThread {
+                        Toast.makeText(activity, "서버 처리 에러",  Toast.LENGTH_SHORT).show()
+                    }
+                }
+                else{
+                    activity?.runOnUiThread {
+                        Toast.makeText(activity, "컴퓨터 종료",  Toast.LENGTH_SHORT).show()
                     }
                 }
 
